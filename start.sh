2467e25c44aac939b915001b8ea0682db70788dc
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2023-12-02 20:27:07 +0800

    fix start script

--- start.sh
+++ start.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-APP_DIR="$(dirname "$SCRIPT_DIR")"/app
+APP_DIR="$SCRIPT_DIR"/app
 CACHE_DIR="$HOME/.cache"
 
-if [ -n "$(docker images -q musicgen-stream)" ]
-then 
+if [ -z "$(docker images -q musicgen-stream)" ]
+then
     bash "$SCRIPT_DIR"/build.sh
 fi
 
+model=$1
+
+if [ -z "$model" ]
+then
+    model="facebook/musicgen-small"
+fi
+
 docker run -it --rm \
-    --gpus=all \
     --name "$APP_NAME" \
     -p 8765:8765 \
     -v "$APP_DIR":/app \
     -v "$CACHE_DIR":/root/.cache \
     -v "$CACHE_DIR"/huggingface/:/root/.cache/huggingface \
-    -e MODEL_NAME="$1"
+    -e MODEL_NAME="$model" \
     musicgen-stream
