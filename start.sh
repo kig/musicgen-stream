6ed389dc09637d85de50a458297086491dc98f8d
Ilmari Heikkinen <hei@heichen.hk>
2023-12-04 14:58:53 +0800

    start.sh set container name, auto-detect CUDA

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 APP_DIR="$SCRIPT_DIR"/app
 CACHE_DIR="$HOME/.cache"
+APP_NAME="$(basename "$SCRIPT_DIR")"
 
 if [ -z "$(docker images -q musicgen-stream)" ]
 then
@@ -16,11 +17,21 @@ then
     model="facebook/musicgen-small"
 fi
 
+GPU_ENABLED=""
+if [ ! -z "$(which nvidia-smi)" ]
+then
+    GPU_ENABLED="--gpus=all"
+    if [ ! -z "$2" ]
+    then
+        GPU_ENABLED="--gpus=device=$2"
+    fi
+fi
+
 docker run -it --rm \
+    $GPU_ENABLED \
     --name "$APP_NAME" \
     -p 8765:8765 \
     -v "$APP_DIR":/app \
-    -v "$CACHE_DIR":/root/.cache \
     -v "$CACHE_DIR"/huggingface/:/root/.cache/huggingface \
     -e MODEL_NAME="$model" \
     musicgen-stream
